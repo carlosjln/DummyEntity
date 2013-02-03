cb0a2e9d57dbb984e4dc02fb98597623153c2eb9
carlosjln <carlosjln01@gmail.com>
2013-02-02 20:33:01 -0430

    adding build event to create a nuget package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "DummyEntity" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "Just some DummyEntity ^_^" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Carlos J. López - @carlosjln" )]
+[assembly: AssemblyCompany( "Carlos J. López - twitter/carlosjln" )]
 [assembly: AssemblyProduct( "DummyEntity" )]
-[assembly: AssemblyCopyright( "Copyright ©  2012" )]
+[assembly: AssemblyCopyright( "Copyright © 2012" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "1.0.*" )]
 [assembly: AssemblyFileVersion( "1.0.0.0" )]
